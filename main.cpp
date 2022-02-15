83b1405f2ed057d8311a21cb4e959acba8738a68
Nikita Hismatov <hismatovns@gmail.com>
2022-02-15 09:59:47 +0500

    formatting

--- main.cpp
+++ main.cpp
@@ -1,34 +1,27 @@
-#include <iostream>
 #include <args.hxx>
+#include <iostream>
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     args::ArgumentParser parser("vcpkg repositories stats loader");
     args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
     args::Positional<std::string> vcpkg_root(parser, "vcpkg_root", "where README.md is :)");
 
-    try
-    {
+    try {
         parser.ParseCLI(argc, argv);
-    }
-    catch (args::Help&)
-    {
+    } catch (args::Help &) {
         std::cout << parser;
         return 0;
-    }
-    catch (args::ParseError& e)
-    {
+    } catch (args::ParseError &e) {
         std::cerr << e.what() << std::endl;
         std::cerr << parser;
         return 1;
-    }
-    catch (args::ValidationError& e)
-    {
+    } catch (args::ValidationError &e) {
         std::cerr << e.what() << std::endl;
         std::cerr << parser;
         return 2;
     }
 
-    if(!vcpkg_root) {
+    if (!vcpkg_root) {
         std::cout << "No vcpkg_root specified, sorry" << std::endl;
         return 3;
     }
