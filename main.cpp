f7a9c9a7c66e681b1b07fb2086109147509b6708
Nikita Hismatov <hismatovns@gmail.com>
2022-02-15 10:34:29 +0500

    ports_path

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <args.hxx>
+#include <filesystem>
 #include <iostream>
 
 int main(int argc, char **argv) {
     args::ArgumentParser parser("vcpkg repositories stats loader");
     args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
-    args::Positional<std::string> vcpkg_root(parser, "vcpkg_root", "where README.md is :)");
+    args::Positional<std::filesystem::path> vcpkg_root_arg(parser, "vcpkg_root_arg", "where README.md is :)");
 
     try {
         parser.ParseCLI(argc, argv);
@@ -21,9 +22,26 @@ int main(int argc, char **argv) {
         return 2;
     }
 
-    if (!vcpkg_root) {
-        std::cout << "No vcpkg_root specified, sorry" << std::endl;
+    if (!vcpkg_root_arg) {
+        std::cerr << "No vcpkg_root_arg specified, sorry" << std::endl;
         return 3;
+    }
+
+    const auto vcpkg_root = args::get(vcpkg_root_arg);
+
+    using namespace std::filesystem;
+
+    if (!exists(vcpkg_root)) {
+        std::cerr << vcpkg_root << " does not exist!" << std::endl;
+        return 4;
+    }
+
+    const auto ports_path = canonical(vcpkg_root) / "ports";
+
+    if (!exists(ports_path)) {
+        std::cerr << "Something wrong with your copy of vcpkg - "
+                  << "it does not contain `ports` subdirectory" << std::endl;
+        return 5;
     }
 
     return 0;
